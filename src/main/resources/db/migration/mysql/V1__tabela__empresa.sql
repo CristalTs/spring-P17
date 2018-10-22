547afbf1b60480313aa094655d0967bac0642927
CristalTs <cristaltshishimbi@yahoo.fr>
2018-10-21 23:17:21 -0200

    atualizando arquivo de migration

--- src/main/resources/db/migration/mysql/V1__tabela__empresa.sql
+++ src/main/resources/db/migration/mysql/V1__tabela__empresa.sql
@@ -3,7 +3,7 @@ CREATE TABLE `empresa` (
 	`cnpj` varchAr(255) NOT NULL,
 	`data_atualizacao` datetime NOT NULL,
 	`data_criacao` datetime NOT NULL,
-	`razao_social` varchar(255) NOT NULL,
+	`razao_social` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `empresa`
